b3b230c52e83578c3632cf5c3105d78d6fd94312
GabrieleCalarota <gabriele.calarota@gmail.com>
2016-08-11 19:55:17 +0200

    Add files via upload

---
+++ ProgettoFisica/ProgettoFisica - Copia/Mylibrary.hpp
@@ -0,0 +1,23 @@
+#ifndef MYLIBRARY_HPP_
+#define MYLIBRARY_HPP_
+#include <iostream>
+using namespace std;
+#include <stdio.h>
+#include <cmath>
+#include <cstring>
+#include <termios.h>
+#include <iomanip>
+#include <unistd.h>
+#include <fcntl.h>
+#include <ctime>
+const double PI=3.141592654;
+static struct termios old, nuovo;
+void initTermios(int echo);
+void resetTermios(void);
+char getch_(int echo);
+int kbhit(void);
+char getch(void);
+void clear_screen();
+
+
+#endif
